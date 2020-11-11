b7a9a7fbe2928e3bb3054edf83a845b6c1c4129f
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-11-11 18:00:42 +0000

    done

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo') (session);
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -22,6 +24,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+app.use(
+    session({
+      secret: 'my secret',
+      cookie: { maxAge: 60000}, //time for the session to be open without being moved
+      rolling: true, // session expires unless you're moving around on the wedsite
+      store: new MongoStore({ // store session information in mongo 
+        mongooseConnection: mongoose.connection
+      }) 
+    })
+  )
 
 // Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
